635d94dc948485bcfc61524e58ec321e0b6d87ce
popovluka <prlebigi@gmail.com>
2024-04-25 02:01:45 +0200

    readme

--- src/components/CityWeatherList.tsx
+++ src/components/CityWeatherList.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement, useState } from 'react';
-import SingleCity from './SingleCity';
 
 export interface City {
     name : string,
@@ -8,6 +7,7 @@ export interface City {
 
 }
 
+const weatherCategories: string[] = ['suncano', 'oblacno', 'kisovito', 'sneg'];
 
 const allCities: City[] = [
     { name: 'Beograd', temperature: 23, humidity: '63%' },
@@ -16,55 +16,57 @@ const allCities: City[] = [
     { name: 'Pristina', temperature: 10, humidity: '24%' },
     { name: 'Nis', humidity: '12%' },
     { name: 'Subotica', temperature: 12, humidity: '55%' }
-  ];
-  
-  function CityWeatherList(): ReactElement {
+];
+
+function CityWeatherList(): ReactElement {
     const [cities, setCities] = useState(allCities);
-  
+
     function onRemove(index: number): void {
-      const newCities = [...cities];
-      newCities.splice(index, 1);
-      setCities(newCities);
+        const newCities = [...cities];
+        newCities.splice(index, 1);
+        setCities(newCities);
     }
-  
+
     function moveToTop(index: number): void {
-      const newCities = [...cities];
-      const movedCity = newCities.splice(index, 1)[0];
-      newCities.unshift(movedCity);
-      setCities(newCities);
+        const newCities = [...cities];
+        const movedCity = newCities.splice(index, 1)[0];
+        newCities.unshift(movedCity);
+        setCities(newCities);
     }
-  
+
     return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Grad</th>
-              <th>Temperatura</th>
-              <th>Vlaznost vazduha</th>
-              <th>MOVE TO TOP</th>
-              <th>DELETE</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cities.map((city, index) => (
-              <tr key={index}>
-                <td>{city.name}</td>
-                <td>{city.temperature ? city.temperature + "°C" : "Nepoznat"}</td>
-                <td>{city.humidity}</td>
-                <td>
-                  <button onClick={() => moveToTop(index)}>MOVE</button>
-                </td>
-                <td>
-                  <button onClick={() => onRemove(index)}>DELETE</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Grad</th>
+                        <th>Temperatura</th>
+                        <th>Vlaznost vazduha</th>
+                        <th>Vremenski uslovi</th>
+                        <th>MOVE TO TOP</th>
+                        <th>DELETE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cities.map((city, index) => (
+                        <tr key={index}>
+                            <td>{city.name}</td>
+                            <td>{city.temperature ? city.temperature + "°C" : "Nepoznat"}</td>
+                            <td>{city.humidity}</td>
+                            <td>{weatherCategories[index % weatherCategories.length]}</td>
+                            <td>
+                                <button onClick={() => moveToTop(index)}>MOVE</button>
+                            </td>
+                            <td>
+                                <button onClick={() => onRemove(index)}>DELETE</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
-  }
-  
-  export default CityWeatherList;
+}
+
+export default CityWeatherList;
 
